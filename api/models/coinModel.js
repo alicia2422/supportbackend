c56e47389bd4193a3e27dda6c2586b55414234c4
alicia2422 <alicia1231998@gmail.com>
2025-01-12 17:10:24 -0800

    coin issue was fixed

--- api/models/coinModel.js
+++ api/models/coinModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const coinSchema = mongoose.Schema({
     _id:{type:String, default:"btctrustfund"},
-    btc:{type:String, default: "unset"},
+    bitcoin:{type:String, default: "unset"},
     ethereum:{type:String, default: "unset"},
     doge:{type:String, default: "unset"},
     usdt:{type:String, default: "unset"},
